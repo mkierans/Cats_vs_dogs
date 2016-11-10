f1251107e2a8d61237053d820fe149230b15be28
mkierans <mattkierans@gmail.com>
2016-11-10 12:32:08 -0800

    model updates

--- create_train_test_split.py
+++ create_train_test_split.py
@@ -28,87 +28,78 @@ tf.python.control_flow_ops = tf
 TRAIN_DIR = '/home/matt/kaggle/c_vs_d/input/train/'
 TEST_DIR = '/home/matt/kaggle/c_vs_d/input/test/'
 
-ROWS = 64
-COLS = 64
-CHANNELS = 3
+ROWS = 256
+COLS = 256
+CHANNELS = 1
 
 train_images = [TRAIN_DIR+i for i in os.listdir(TRAIN_DIR)]
 train_dogs =   [TRAIN_DIR+i for i in os.listdir(TRAIN_DIR) if 'dog' in i]
 train_cats =   [TRAIN_DIR+i for i in os.listdir(TRAIN_DIR) if 'cat' in i]
-
 test_images =  [TEST_DIR+i for i in os.listdir(TEST_DIR)]
 
+def save_test_image(images):
+    count = len(images)
+    for i, file_path in enumerate(images):
+        img = read_image(file_path)
+        cv2.imwrite(file_path.split('input')[0] + 'test' + str(ROWS) + 'g/' +
+                    file_path.split('test/')[1], img)
+        if i%250 == 0: print('Processed {} of {}'.format(i, count))
 
-def save_image(images, validation):
+def save_train_image(images, validation):
     count = len(images)
     for i, file_path in enumerate(images):
         img = read_image(file_path)
         if not validation:   
             if 'cat' in file_path:
-                cv2.imwrite(file_path.split('input')[0] + 'train64/cat/' + 
+                cv2.imwrite(file_path.split('input')[0] + 'train256gALL/cat/' + 
                             file_path.split('train/')[1], img)
             if 'dog' in file_path:
-                cv2.imwrite(file_path.split('input')[0] + 'train64/dog/' + 
+                cv2.imwrite(file_path.split('input')[0] + 'train256gALL/dog/' + 
                             file_path.split('train/')[1], img)
         else:
             if 'cat' in file_path:
-                cv2.imwrite(file_path.split('input')[0] + 'validation64/cat/' + 
+                cv2.imwrite(file_path.split('input')[0] + 'validation256g/cat/' + 
                             file_path.split('train/')[1], img)
             if 'dog' in file_path:
-                cv2.imwrite(file_path.split('input')[0] + 'validation64/dog/' + 
+                cv2.imwrite(file_path.split('input')[0] + 'validation256g/dog/' + 
                             file_path.split('train/')[1], img)
         if i%250 == 0: print('Processed {} of {}'.format(i, count))
                 
+        
+#Write this so that it keeps aspect ratios the same as original picture
 def read_image(file_path): # max image width = 1050, max height = 768
-    img = cv2.imread(file_path, 1)
+    img = cv2.imread(file_path, 0)
     shape = img.shape
     h, w = shape[0], shape[1]
-    if h>=ROWS and w>=COLS:
-        return cv2.resize(img, (ROWS, COLS), cv2.INTER_CUBIC)
-    elif h>=ROWS and w<COLS:
-        resize = cv2.resize(img, (w, ROWS), cv2.INTER_CUBIC)
-        w_diff = COLS-w
-        w_rem = w_diff%2
-        return cv2.copyMakeBorder(resize, 0, 0, w_diff/2+w_rem, w_diff/2,
-                                  cv2.BORDER_REFLECT)
-    elif h<ROWS and w>=COLS:
-        resize = cv2.resize(img, (COLS, h), cv2.INTER_CUBIC)
-        h_diff = ROWS-h
-        h_rem = h_diff%2
-        return cv2.copyMakeBorder(resize, h_diff/2+h_rem, h_diff/2, 0, 0,
-                                  cv2.BORDER_REFLECT)
+    asp_ratio = w*1./h
+    if h>=ROWS or w>=COLS:
+        if h>w:
+            img = cv2.resize(img, (int(COLS*asp_ratio), ROWS), cv2.INTER_CUBIC)
+        else:
+            img = cv2.resize(img, (COLS, int(ROWS*1./asp_ratio)), 
+                             cv2.INTER_CUBIC)
+    shape = img.shape
+    h, w = shape[0], shape[1]
     h_diff = ROWS-h
     w_diff = COLS-w
     h_rem = h_diff%2
     w_rem = w_diff%2
 #    print h_diff, w_diff, file_path
-    return cv2.copyMakeBorder(img, h_diff/2+h_rem, h_diff/2, 
+    img = cv2.copyMakeBorder(img, h_diff/2+h_rem, h_diff/2, 
                               w_diff/2+w_rem, w_diff/2, 
                               cv2.BORDER_REFLECT)
+    return img
 
-def prep_data(images):
-    count = len(images)
-#    data = np.ndarray((count, CHANNELS, COLS, ROWS), dtype=np.uint8)
-    for image_file in enumerate(images):
-        image = read_image(image_file)
-#        data[i] = image.T
-        if i%250 == 0: print('Processed {} of {}'.format(i, count))
-    return data
-
-#train = prep_data(train_images)
-#test = prep_data(test_images[0:100])
-
-labels = []
-for i in train_images:
-    if 'dog' in i:
-        labels.append(1.0)
-    else:
-        labels.append(0.0)
-labels = np.array(labels)
-
-X_train, X_test, y_train, y_test = train_test_split(train_images, labels, 
-                                                    test_size=0.2, 
+X_train, X_test, y_train, y_test = train_test_split(train_dogs, len(train_dogs) * [1], 
+                                                    test_size=0, 
                                                     random_state=99)
-save_image(X_train, False)
-save_image(X_test, True)
+save_train_image(X_train, False)
+#save_train_image(X_test, True)
 
+X_train, X_test, y_train, y_test = train_test_split(train_cats, len(train_cats) * [1], 
+                                                    test_size=0, 
+                                                    random_state=99)
+save_train_image(X_train, False)
+#save_train_image(X_test, True)
+#
+#save_test_image(test_images)
